4ef6d872d56bac55e1d40885e40ae172607f45b9
Kieran O Donoghue <kgodonoghue@hotmail.com>
2020-05-17 12:29:40 +0100

    Booking count added

--- Court_Usage_rev01.py
+++ Court_Usage_rev01.py
@@ -92,3 +92,37 @@ for j in pages_to_keep:
 with open('ww' + str(i) + '_Court_Usage_Report_' + str(date_range) + '_.pdf', 'wb') as f:
     output.write(f)
     
+    
+df_access=pd.read_csv('court_usage_ww27.csv',encoding = "ISO-8859-1")
+df_access=df_access.join(df_access['opponents'].str.split(',', 2, expand=True).rename(columns={0:'Opponent 1', 1:'Opponent 2',2:'Opponent 3'}))
+access_counts_1=df_access['booked_by'].value_counts()
+access_counts_2=df_access['Opponent 1'].value_counts()
+access_counts_3=df_access['Opponent 2'].value_counts()
+access_counts_4=df_access['Opponent 3'].value_counts()
+access_counts_1.to_csv('primary_booking_counts1.csv',index=True,header=True)
+access_counts_2.to_csv('primary_booking_counts2.csv',index=True,header=True)
+access_counts_3.to_csv('primary_booking_counts3.csv',index=True,header=True)
+access_counts_4.to_csv('primary_booking_counts4.csv',index=True,header=True)
+access1=pd.read_csv('primary_booking_counts1.csv',encoding = "ISO-8859-1")
+access2=pd.read_csv('primary_booking_counts2.csv',encoding = "ISO-8859-1")
+access3=pd.read_csv('primary_booking_counts3.csv',encoding = "ISO-8859-1")
+access4=pd.read_csv('primary_booking_counts4.csv',encoding = "ISO-8859-1")     
+df_new1=df_access['booked_by']
+df_new2=df_access['Opponent 1']
+df_new3=df_access['Opponent 2']
+df_new4=df_access['Opponent 3']
+df_new=df_new1.append(df_new2)
+df_new=df_new.append(df_new3)
+df_new=df_new.append(df_new4)
+df_new.to_csv('all_counts.csv',index=True,header=True)
+df_booking_all=pd.read_csv('all_counts.csv',encoding = "ISO-8859-1")
+df_booking_all=df_booking_all.rename(columns={"Unnamed: 0": "Count", "0": "Name"})
+booking_counts=df_booking_all['Name'].value_counts()
+booking_counts.to_csv('all_counts_final.csv',index=True,header=True)  
+df_count_final=pd.read_csv('all_counts_final.csv',encoding = "ISO-8859-1")
+df_count_final=df_count_final.rename(columns={"Unnamed: 0": "Name", "Name": "Count"})
+df_count_final=df_count_final[df_count_final['Name'] != 'CLOSED']
+df_count_final=df_count_final[df_count_final['Name']!='CLUB CLOSED']
+df_count_final.to_csv('all_counts_final.csv',index=True,header=True)  
+   
+    
